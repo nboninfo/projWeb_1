1a98863d0225f309c53aafd2f682afba7d910c53
Nuno Batista <nbon.informatica@gmail.com>
2023-08-27 09:22:17 +0100

    Rota para pequisar livros por seu titulo

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Ruta para realizar pesquisa a partir de um titulo que se envia desde o cliente.
+@app.route('/pesquisar', methods=['POST'])
+def pesquisar():
+    titulo = request.form.get('title')
+    dados = {
+        'tirulo': titulo
+    }
+
+    return jsonify(dados)
